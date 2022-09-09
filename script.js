5840b9839578e1dd3cd9993885ba422c73d421ab
Armando Endzelis <endzelisp@gmail.com>
2022-09-09 14:35:59 -0400

    Create different event listeners for readability

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const display = document.querySelector('.display-container #display')
 
 
 
+// isNumber capture a string and compare it against a regular expression
+// to determine if it is a number between the valid range
+
 function isNumber (num) {
   sequence = new RegExp(/[0-9]/g)
   return sequence.test(num)
 }
 
 
-let currentNumValue;
 let currentStringValue = ''
 
-// main event
+// Event listeners
 
 addEventListener('pointerdown', (e) => {
   let target = e.target;
@@ -21,6 +23,10 @@ addEventListener('pointerdown', (e) => {
     currentStringValue += target.textContent;
     display.textContent = currentStringValue;
   }
+})
+
+addEventListener('pointerdown', (e) => {
+  let target = e.target;
 
   if (target.id === 'clear' && target.nodeName === 'BUTTON') {
     display.textContent = 0;
